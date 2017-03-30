8e14326224ed812715580707afab4d6f2c1e39fd
Blake Scherschel <Blake.scherschel@gmail.com>
2017-03-30 18:44:23 -0400

    Initial push for a demo site. Simple two pages with shared template. Start of DBConnection. So far only initiates connection.

---
+++ CollegeEvents/AccessLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CollegeEvents
+{
+    public enum AccessLevel
+    {
+        Student = 0,
+        Admin = 1,
+        SuperAdmin = 2
+    }
+}
---
+++ CollegeEvents/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CollegeEvents
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public AccessLevel GrantedAccess { get; set; }
+    }
+}
